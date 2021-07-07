ea9d03368d5d59c0e755fc0b3d60086e01237c69
id <id@IN0013471>
2021-07-07 11:57:02 +0900

    クラス名変更

--- src/main/java/com/example/demo/loginController.java
+++ src/main/java/com/example/demo/loginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class login {
+public class loginController {
 
 
 
