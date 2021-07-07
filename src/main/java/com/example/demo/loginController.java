a7a6138501de1badc5e639b5b668a7b1b6305623
id <id@IN0013473>
2021-07-07 16:45:36 +0900

    logincontrollerにアカウント登録ページ遷移を追加

--- src/main/java/com/example/demo/loginController.java
+++ src/main/java/com/example/demo/loginController.java
@@ -40,4 +40,22 @@ public class loginController {
 			return mv;
 	}
 
+	//マイメニュー詳細画面へ移動
+		@RequestMapping("/accountPage")
+		public ModelAndView MyPage(
+				ModelAndView mv) {
+			mv.setViewName("accountPage");
+
+			return mv;
+		}
+
+		//マイメニュー（EATボタン押下）
+		@RequestMapping(value = "/regi", method = RequestMethod.POST)
+		public ModelAndView confirm(
+				ModelAndView mv) {
+
+				mv.setViewName("login");
+				return mv;
+		}
+
 }
