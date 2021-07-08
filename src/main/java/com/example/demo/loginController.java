a8647e8b7cb65fa6d0c64392a22fa897429d7901
id <id@IN0013471>
2021-07-08 11:34:26 +0900

    アカウント作成・ログインの実装

--- src/main/java/com/example/demo/loginController.java
+++ src/main/java/com/example/demo/loginController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class loginController {
 
-
+	@Autowired
+	private HttpSession session;
 
 	@RequestMapping("/login")
 	public String log() {
@@ -19,27 +22,38 @@ public class loginController {
 
 	@RequestMapping(value = "/log", method = RequestMethod.POST)
 	public ModelAndView doLogin(
-			@RequestParam("email") String email,
-			@RequestParam("password") String password,
+			@RequestParam(name="email",defaultValue="") String email,
+			@RequestParam(name="password",defaultValue="") String password,
 			ModelAndView mv) {
-		// 名前が空の場合にエラーとする
-		if (email == null || email.length() == 0) {
-			mv.addObject("message", "メールアドレスを入力してください");
-			mv.setViewName("log");
-			return mv;
-		}else if (password == null || password.length() == 0) {
-			mv.addObject("message", "パスワードを入力してください");
-			mv.setViewName("log");
-			return mv;
-		}else if (password == null || password.length() == 0&&email == null || email.length() == 0) {
-		mv.addObject("message", "情報を入力してください");
-		mv.setViewName("log");
-		return mv;
-	}
 
-			mv.setViewName("kcalCal");
-			return mv;
-	}
+		// 名前が空の場合にエラーとする
+		if (email.equals("") || password.equals("")) {
+			mv.addObject("message", "未入力の項目があります。");
+			mv.setViewName("log");
+		} else {
+			user user = (user)session.getAttribute("user");
+
+			String _email = user.getEmail();
+			String _password = user.getPassword();
+
+			if (email.equals(_email) && password.equals(_password)) {
+				//ログイン成功
+				String name = user.getName();
+
+				session.setAttribute("login", name);
+
+				mv.addObject("login", name);
+				mv.addObject("user", user);
+
+				mv.setViewName("kcalCal");
+			} else {
+				mv.addObject("message", "ユーザIDとパスワードが一致しませんでした。");
+				mv.setViewName("log");
+			}
+		}
+		return mv;
+		}
+
 
 	//マイメニュー詳細画面へ移動
 		@RequestMapping("/accountPage")
@@ -50,6 +64,36 @@ public class loginController {
 			return mv;
 		}
 
+		//アカウント作成ボタン
+		@RequestMapping(value="/newUser",method = RequestMethod.POST)
+		public ModelAndView newUser(
+				@RequestParam("name") String name,
+				@RequestParam("email") String email,
+				@RequestParam("password") String password,
+				ModelAndView mv) {
+
+			if (!name.equals("") && !email.equals("") && !password.equals("")) {
+
+				//ユーザーインスタンスの生成
+				user user = new user(name, email, password);
+
+				session.setAttribute("user", user);
+
+				mv.addObject("message", "登録が完了しました。");
+
+				mv.setViewName("log");
+			} else {
+				mv.addObject("message", "未入力の項目があります。");
+				mv.setViewName("accountPage");
+			}
+
+			mv.setViewName("accountpage");
+
+			return mv;
+		}
+
+
+
 		//マイメニュー（EATボタン押下）
 		@RequestMapping(value = "/regi", method = RequestMethod.POST)
 		public ModelAndView confirm(
@@ -58,23 +102,5 @@ public class loginController {
 				mv.setViewName("login");
 				return mv;
 		}
-		//アカウント新規作成
-		@PostMapping("/newUser")
-		public ModelAndView usersadd(
-				ModelAndView mv,
-				@RequestParam(name = "name") String name,
-				@RequestParam(name = "email") String email,
-				@RequestParam(name = "password") String pass) {
-
-			//登録するユーザのエンティティのインスタンスを生成
-			User user = new User(name, email, pass);
-
-			//UserエンティティをUserテーブルに登録
-
-			UserRepository.saveAndFlush(user);
-
-			return users(mv);
-		}
-
 
 }
--- src/main/java/com/example/demo/user.java
+++ src/main/java/com/example/demo/user.java
@@ -42,5 +42,38 @@ public class user {
 		this.password = password;
 	}
 
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 
 }
