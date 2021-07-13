474d57dddd66f12b79795ae4c22aed53bde5255e
id <id@IN0013473>
2021-07-13 09:39:59 +0900

    コントローラ追加

--- src/main/java/com/example/demo/KcalCalCoentroller.java
+++ src/main/java/com/example/demo/KcalCalCoentroller.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -92,101 +90,5 @@ public class KcalCalCoentroller {
 	}
 
 
-	//マイメニューへ移動
-	@RequestMapping("/myMenu")
-	public ModelAndView myMenu(
-			ModelAndView mv) {
-		mv.setViewName("myMenu");
-
-		return mv;
-	}
-
-	//カスタム入力画面へ移動
-	@RequestMapping("/custom")
-	public ModelAndView custom(
-			ModelAndView mv) {
-		mv.setViewName("custom");
-
-		return mv;
-	}
-
-	//登録ボタン押下
-	@PostMapping("/myMenu")
-	public ModelAndView confirm(
-			@RequestParam("menu") String menu,
-			ModelAndView mv) {
-		int dishcode = (int) session.getAttribute("dishcode");
-		dishcode=dishcode+1;
-
-		menu m = new menu(menu,dishcode);
-
-		menuRepository.saveAndFlush(m);
-
-		List<menu> m2= menuRepository.findAll();
-		mv.addObject("list",m2);
-		mv.setViewName("myMenu");
-		return mv;
-	}
-	//メニュー登録（登録ボタン押下）
-/*	@RequestMapping(value = "/myMenu", method = RequestMethod.POST)
-	public ModelAndView confirm(
-			@RequestParam("dishname") String dishname,
-			@RequestParam("uname") String uname,
-			@RequestParam("kcalall") Integer kcalall,
-			@RequestParam("grams") Integer grams,
-			@RequestParam("kcal") Integer kcal,
-			ModelAndView mv) {
-
-		mylists mylists = new mylists(dishname,uname,kcalall,grams,kcal);
-
-		mylistsRepository.saveAndFlush(mylists);
-
-		mv.addObject("list", mylists);
-
-		mv.setViewName("myMenu");
-		return mv;
-	}*/
-
-
-	//カスタム入力（登録ボタン押下）
-	@RequestMapping(value = "/custom/regi", method = RequestMethod.POST)
-	public ModelAndView customRegi(
-			@RequestParam("dishname") String dishname,
-			@RequestParam("kcal") String kcal,
-			ModelAndView mv
-			) {
-		if (!dishname.equals("") && !kcal.equals("")) {
-
-			//ユーザーインスタンスの生成
-			mylists mylists = new mylists(dishname, Integer.parseInt(kcal));
-
-			mylistsRepository.saveAndFlush(mylists);
-
-
-			mv.addObject("message", "登録が完了しました。");
-
-			mv.setViewName("custom");
-		} else {
-			mv.addObject("message", "未入力の項目があります。");
-			mv.setViewName("custom");
-		}
-		return mv;
-	}
-	//カスタム登録（EATボタン押下）
-	@RequestMapping(value = "/custom/confirm", method = RequestMethod.POST)
-	public ModelAndView customConfirm(
-			ModelAndView mv) {
-
-		mv.setViewName("confirm");
-		return mv;
-	}
-	//マイメニュー登録（登録ボタン押下）
-	@RequestMapping(value = "/Mymenu/regi", method = RequestMethod.POST)
-	public ModelAndView menuConfirm(
-			ModelAndView mv) {
-
-		mv.setViewName("myMenu");
-		return mv;
-	}
 
 }
